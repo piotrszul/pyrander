95c2ea9a2fa6cbc67fd534fe38609f43d40a1fdb
Piotr Szul <piotr.szul@csiro.au>
2018-01-31 19:03:13 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from distutils.core import setup
 setup(
   name = 'pyrander',
   packages = ['pyrander'], # this must be the same as the name above
-  version = '0.0.1',
+  version = '0.0.2.dev1',
   description = 'A random test lib',
   author = 'Piotr Szul',
   author_email = 'piotr.szul@csiro.au',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  url = 'https://github.com/piotrszul/pyrander',
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
 )
