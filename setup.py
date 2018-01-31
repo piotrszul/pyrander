7413c263005bc12e2cff91c021d14510576a59ef
Piotr Szul <piotr.szul@csiro.au>
2018-01-31 18:48:43 +0100

    Initial version

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyrander',
+  packages = ['pyrander'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'A random test lib',
+  author = 'Piotr Szul',
+  author_email = 'piotr.szul@csiro.au',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
