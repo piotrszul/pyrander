cb00a53b9fef6b14317245679f24990baa44fd01
Piotr Szul <piotr.szul@csiro.au>
2018-01-31 20:22:03 +0100

    added build tagging

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from distutils.core import setup
+from setuptools import setup 
 setup(
   name = 'pyrander',
   packages = ['pyrander'], # this must be the same as the name above
-  version = '0.0.2.dev1',
+  version = '0.0.2',
   description = 'A random test lib',
   author = 'Piotr Szul',
   author_email = 'piotr.szul@csiro.au',
   url = 'https://github.com/piotrszul/pyrander',
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
+  license = "MIT",
 )
